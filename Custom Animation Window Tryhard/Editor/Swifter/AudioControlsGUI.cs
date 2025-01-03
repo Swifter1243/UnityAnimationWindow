f462d45604a4abfa285cab025206bdbbad9bd832
Swifter <61858676+Swifter1243@users.noreply.github.com>
2025-01-02 21:50:20 -0500

    tooltips + editor dragging on settings properties

--- Custom Animation Window Tryhard/Editor/Swifter/AudioControlsGUI.cs
+++ Custom Animation Window Tryhard/Editor/Swifter/AudioControlsGUI.cs
@@ -22,6 +22,29 @@ class AudioControlsGUI
         },
         fixedHeight = 20
     };
+    
+    private static GUIContent s_AudioEnabledField =
+        new GUIContent("Audio Enabled", "Whether to enable audio syncing with the animation.");
+    private static GUIContent s_AudioClipField =
+        new GUIContent("Audio Clip", "The audio clip to play in sync with the animation.");
+    private static GUIContent s_BpmField =
+        new GUIContent("BPM", "The beats per minute of the audio.");
+    private static GUIContent s_WaveformColorField =
+        new GUIContent("Waveform Color", "The color of the waveform for the audio visualization.");
+    
+    private static GUIContent s_BpmGuideField =
+        new GUIContent("BPM Guide Enabled", "Whether to enable BPM guides.");
+    private static GUIContent s_BeatPrecisionField =
+        new GUIContent("Beat Precision", "The amount of in-between beats for the BPM guides.");
+    private static GUIContent s_GuideColorField =
+        new GUIContent("Guide Color", "The color of the BPM guides.");
+    private static GUIContent s_BeatLabelsField =
+        new GUIContent("Beat Labels", "Whether the BPM guides should be numbered.");
+    private static GUIContent s_LatencyCompensationField =
+        new GUIContent("Latency Compensation", "Compensate for audio latency in milliseconds.");
+    
+    private static GUIContent s_AudioOffsetField =
+        new GUIContent("Offset", "How much to offset the audio by (in beats).");
 
     private float _indent = 0;
 
@@ -67,7 +90,7 @@ class AudioControlsGUI
         VerticalSpace();
 
         BeginHorizontal();
-        audioControls.m_isAudioEnabled = GUILayout.Toggle(audioControls.m_isAudioEnabled, "Audio Enabled");
+        audioControls.m_isAudioEnabled = EditorGUILayout.Toggle(s_AudioEnabledField, audioControls.m_isAudioEnabled);
         EndHorizontal();
 
         if (audioControls.m_isAudioEnabled)
@@ -85,18 +108,16 @@ class AudioControlsGUI
         VerticalSpace();
 
         BeginHorizontal();
-        GUILayout.Label("Audio Clip: ");
-        audioControls.m_audioClip = EditorGUILayout.ObjectField(audioControls.m_audioClip, typeof(AudioClip), false) as AudioClip;
+        audioControls.m_audioClip = EditorGUILayout.ObjectField(s_AudioClipField, audioControls.m_audioClip, typeof(AudioClip), false) as AudioClip;
         EndHorizontal();
 
         BeginHorizontal();
-        GUILayout.Label("Waveform Color: ");
-        audioControls.m_waveformColor = EditorGUILayout.ColorField(audioControls.m_waveformColor);
+        audioControls.m_waveformColor = EditorGUILayout.ColorField(s_WaveformColorField, audioControls.m_waveformColor);
         EndHorizontal();
 
         VerticalSpace();
         BeginHorizontal();
-        audioControls.m_bpmGuideEnabled = GUILayout.Toggle(audioControls.m_bpmGuideEnabled, "BPM Guide");
+        audioControls.m_bpmGuideEnabled = EditorGUILayout.Toggle(s_BpmGuideField, audioControls.m_bpmGuideEnabled);
         EndHorizontal();
 
         if (audioControls.m_bpmGuideEnabled)
@@ -142,8 +163,7 @@ class AudioControlsGUI
     private void AudioOffsetOptions(AudioOffsetContainer audioOffsetContainer)
     {
         BeginHorizontal();
-        GUILayout.Label("Offset: ");
-        audioOffsetContainer.offset = EditorGUILayout.FloatField(audioOffsetContainer.offset);
+        audioOffsetContainer.offset = EditorGUILayout.FloatField(s_AudioOffsetField, audioOffsetContainer.offset);
         EndHorizontal();
     }
 
@@ -152,28 +172,23 @@ class AudioControlsGUI
         VerticalSpace();
 
         BeginHorizontal();
-        GUILayout.Label("BPM: ");
-        audioControls.m_bpm = EditorGUILayout.FloatField(audioControls.m_bpm);
+        audioControls.m_bpm = EditorGUILayout.FloatField(s_BpmField, audioControls.m_bpm);
         EndHorizontal();
         
         BeginHorizontal();
-        GUILayout.Label("Beat Precision: ");
-        audioControls.m_bpmGuidePrecision = EditorGUILayout.IntField(audioControls.m_bpmGuidePrecision);
+        audioControls.m_bpmGuidePrecision = EditorGUILayout.IntField(s_BeatPrecisionField, audioControls.m_bpmGuidePrecision);
         EndHorizontal();
 
         BeginHorizontal();
-        GUILayout.Label("Guide Color: ");
-        audioControls.m_bpmGuideColor = EditorGUILayout.ColorField(audioControls.m_bpmGuideColor);
+        audioControls.m_bpmGuideColor = EditorGUILayout.ColorField(s_GuideColorField, audioControls.m_bpmGuideColor);
         EndHorizontal();
         
         BeginHorizontal();
-        GUILayout.Label("Beat Labels: ");
-        audioControls.m_showBeatLabels = EditorGUILayout.Toggle(audioControls.m_showBeatLabels);
+        audioControls.m_showBeatLabels = EditorGUILayout.Toggle(s_BeatLabelsField, audioControls.m_showBeatLabels);
         EndHorizontal();
         
         BeginHorizontal();
-        GUILayout.Label("Latency Compensation (ms): ");
-        audioControls.m_latencyMilliseconds = EditorGUILayout.IntField(audioControls.m_latencyMilliseconds);
+        audioControls.m_latencyMilliseconds = EditorGUILayout.IntField(s_LatencyCompensationField, audioControls.m_latencyMilliseconds);
         EndHorizontal();
     }
 }
